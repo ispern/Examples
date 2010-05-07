d6d457084277d9a1967d38221d2aadb61e1c2431
matsuoka@ispern.com <hiromitsu@MacBook.local>
2010-05-08 02:33:50 +0900

    既存ソースの追加

---
+++ src/Panel/TreePanel2.js
@@ -0,0 +1,24 @@
+Ext.onReady( function() {
+	Ext.BLANK_IMAGE_URL = 'http://extjs.cachefly.net/ext-3.2.0/resources/images/default/s.gif';
+	Ext.QuickTips.init();
+
+	// rootのツリーノードを生成
+	var root = new Ext.tree.AsyncTreeNode({
+		text: 'forum',
+		leaf: false
+	});
+
+	var treePanel = new Ext.tree.TreePanel({
+		title: 'Tree Panel',		// PanelのTitleを指定
+		renderTo: Ext.getBody(),	// 表示する要素を指定
+		width: 225,					// 幅
+		height: 200,				// 高さ
+		minSize: 150,				// 最小の幅
+		maxSize: 300,				// 最大の幅
+		rootVisible: false,			// rootのツリーノードを表示するか
+		root: root,					// rootのツリーノードを指定
+		loader: new Ext.tree.TreeLoader({
+			url: '../../data/TreePanel.json'	/* 取得元URLを指定 */
+		})
+	});
+});
