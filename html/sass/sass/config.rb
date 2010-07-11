4679cfee887ba3a46e1de311573bacc1d31c2468
matsuoka@ispern.com <hiromitsu@MacBook.local>
2010-07-11 10:45:30 +0900

    GeoTweet AnyWhere APIを使ってサインイン→ツイート表示までとりあえず完成

---
+++ html/sass/sass/config.rb
@@ -0,0 +1,6 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+css_dir = "../css"
+sass_dir = "../sass"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
---
+++ src/GeoTweets/TimelineView.js
@@ -0,0 +1,75 @@
+Ext.ns('Ext.Twitter');
+
+Ext.regModel('Tweet', {
+	fields: [
+		{name: 'profileImageUrl', mapping: 'user.profileImageUrl'},
+		{name: 'text'},
+		{name: 'name', mapping: 'user.name'}
+	]
+});
+
+Ext.Twitter.TimelineView = Ext.extend(Ext.DataView, {
+
+	tpl: [
+		'<tpl for=".">',
+			'<div class="tweet">',
+				'<img src="{user.profileImageUrl}">',
+				'<div class="tweet-anchor"></div>',
+				'<div class="tweet-content ' + (Ext.platform.isPhone ? 'x-phone' : '') + '">',
+					'<h2>{user.name}</h2>',
+					'<p>{text}</p>',
+				'</div>',
+			'</div>',
+		'</tpl>'
+	].join(''),
+
+
+	initComponent: function() {
+		var me = this;
+
+		// 設定適用
+		Ext.apply(me, {
+			store: new Ext.data.Store({
+				model: 'Tweet'
+			}),
+			emptyText: '<div class="no-tweet">No Tweet.</div>',
+			itemSelector: 'div.tweet'
+		});
+
+		// スーパークラスメソッドコール
+		Ext.Twitter.TimelineView.superclass.initComponent.call(me);
+	},
+
+	initEvents: function() {
+		var me = this;
+
+		// スーパークラスメソッドコール
+		Ext.Twitter.TimelineView.superclass.initEvents.call(me);
+
+		// レンダリング後のイベント定義
+		me.on('afterrender', me.onAfterRender, me);
+	},
+
+	onAfterRender: function() {
+		var me = this;
+		console.log(me.rendered);
+		me.refresh();
+	},
+
+	loadData: function(tl) {
+		var me = this;
+
+		if (!tl || !tl.each) {
+			return false;
+		}
+
+		var store = me.getStore();
+		tl.each(function(s) {
+			store.loadData([s], true);
+		});
+
+		return true;
+	}
+});
+
+Ext.reg('timeline', Ext.Twitter.TimelineView);
