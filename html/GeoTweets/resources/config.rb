540e25c3580984bead6fbb09a1f33bde05af250d
ispern <matsuoka@ispern.com>
2011-01-09 11:36:50 +0900

    フォルダ構成変更

--- html/GeoTweets/resources/config.rb
+++
@@ -1,8 +0,0 @@
-require 'compass-colors'
-
-# Require any additional compass plugins here.
-images_dir = '../../../lib/sencha-touch/resources/img/'
-css_dir = 'css/'
-sass_dir = 'sass/'
-output_style = :compressed
-environment = :production
