68d900d9281c117788f66c53173082fbb059c3a4
ispern <matsuoka@ispern.com>
2011-04-03 21:50:10 +0900

    中途半端にコミットされたので再コミット

--- extjs/widgets/chart/chart.js
+++ extjs/widgets/chart/chart.js
@@ -1,10 +1,10 @@
 Ext.override(Ext.chart.CartesianChart, {
-    setXAxis : function(value){
+    setXAxis: function(value){
         var xAxis = this.createAxis('xAxis', value);
         this.swf.setHorizontalAxis(xAxis);
     },
-    setYAxis : function(value){
+    setYAxis: function(value){
         var yAxis = this.createAxis('yAxis', value);
-        this.swf.setVerticalAxis();
+        this.swf.setVerticalAxis(yAxis);
     }
 });
